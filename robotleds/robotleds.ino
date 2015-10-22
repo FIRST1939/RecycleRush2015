d21dd37f8180e817c15ea59261315799584ce6f0
Joey Holliday <joeyholliday77@gmail.com>
2015-10-22 13:37:08 -0500

    Fix LEDs

---
+++ robotleds/robotleds.ino
@@ -0,0 +1,100 @@
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(59, 6, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
+  Serial.begin(9600);
+}
+
+int val = 0;
+
+void updateValue(){
+  while(Serial.available()>0){
+    byte b = Serial.read();
+    for(int i=0;i<8;i++){
+      if(bitRead(b, i)){
+        val = i; 
+      }
+    } 
+  }
+}
+
+void loop() {
+  updateValue();
+  switch (val) {
+    case 1:
+      theaterChase(strip.Color(127,127,127), 1);
+      break;
+    case 2:
+      rainbowCycle(2);
+      break;
+    case 3:
+      theaterChase(strip.Color(127, 127, 0), 3);
+      break;
+    case 4:
+      theaterChase(strip.Color(0, 0, 127), 4);
+      break;
+    case 5:
+      theaterChase(strip.Color(127, 0, 0), 5);
+      break;
+  }
+}
+
+void rainbowCycle(int value) {
+  uint8_t wait = 20;
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+
+    updateValue();
+    if(value!=val){
+      return;
+    }
+  }
+}
+
+void theaterChase(uint32_t c, int value) {
+  uint8_t wait = 50;
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+
+      updateValue();
+      if(value!=val){
+        return;
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
--- src/org/usfirst/frc/team1939/util/LEDs.java
+++ src/org/usfirst/frc/team1939/util/LEDs.java
@@ -3,44 +3,45 @@ package org.usfirst.frc.team1939.util;
 import org.usfirst.frc.team1939.robot.Robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class LEDs implements Runnable {
 
 	private SerialPort port;
 
+	public LEDs() {
+		try {
+			this.port = new SerialPort(9600, SerialPort.Port.kUSB);
+		} catch (Exception e) {
+			e.printStackTrace();
+			DriverStation.reportError("Couldn't Find Arduino", false);
+		}
+	}
+
 	@Override
 	public void run() {
 		while (true) {
-			if (this.port == null) {
-				try {
-					this.port = new SerialPort(9600, SerialPort.Port.kUSB);
-				} catch (Exception e) {
-					e.printStackTrace();
-					DriverStation.reportError("Couldn't Find Arduino: " + System.currentTimeMillis(), false);
-				}
-			} else {
-				byte b = 0;
-				if (Robot.robot.isEnabled())
-					b = writeBit(b, 1, 0);
-				if (DriverStation.getInstance().isDSAttached())
-					b = writeBit(b, 1, 1);
-				if (DriverStation.getInstance().getAlliance() == Alliance.Blue)
-					b = writeBit(b, 1, 2);
-				if (Robot.robot.isAutonomous())
-					b = writeBit(b, 1, 3);
+			int state = 0;
+			if (!DriverStation.getInstance().isDSAttached()) {
+				state = 1;
+			} else if (!Robot.robot.isEnabled()) {
+				state = 2;
+			} else if (Robot.robot.isAutonomous()) {
+				state = 3;
+			} else if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Blue) {
+				state = 4;
+			} else if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Red) {
+				state = 5;
+			}
 
-				byte[] buffer = new byte[1];
-				buffer[0] = b;
-				this.port.write(buffer, 1);
-				this.port.flush();
-
-				try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+			byte[] buffer = new byte[1];
+			buffer[0] = writeBit(buffer[0], 1, state);
+			this.port.write(buffer, 1);
+			this.port.flush();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
